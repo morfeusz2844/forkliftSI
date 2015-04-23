2ad426b472f6069f24219fc5544acb8dbfc94e8f
t3mm3 <t3llmemore@hotmail.com>
2015-04-23 21:41:38 +0200

    Some simple models added

---
+++ forkliftSI/src/model/Forklift.java
@@ -0,0 +1,55 @@
+package model;
+
+public class Forklift {
+	private final static int MAX_FUEL = 100;
+	private final static int STARTING_POSITION_X = 0;
+	private final static int STARTING_POSITION_Y = 0;
+	private int positionX;
+	private int positionY;
+
+	private int fuelLevel;
+
+	public Forklift() {
+		this.fuelLevel = MAX_FUEL;
+		this.positionX = STARTING_POSITION_X;
+		this.positionY = STARTING_POSITION_Y;
+	}
+
+	public Forklift(int positionX, int positionY, int fuelLevel) {
+		super();
+		this.positionX = positionX;
+		this.positionY = positionY;
+		this.fuelLevel = fuelLevel;
+	}
+
+	public Forklift(int fuelLevel) {
+		this.positionX = STARTING_POSITION_X;
+		this.positionY = STARTING_POSITION_Y;
+		this.fuelLevel = fuelLevel;
+	}
+
+	public int getPositionX() {
+		return positionX;
+	}
+
+	public void setPositionX(int positionX) {
+		this.positionX = positionX;
+	}
+
+	public int getPositionY() {
+		return positionY;
+	}
+
+	public void setPositionY(int positionY) {
+		this.positionY = positionY;
+	}
+
+	public int getFuelLevel() {
+		return fuelLevel;
+	}
+
+	public void setFuelLevel(int fuelLevel) {
+		this.fuelLevel = fuelLevel;
+	}
+
+}
---
+++ forkliftSI/src/model/Warehouse.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Warehouse {
+	private final static int SIZE_X = 15;
+	private final static int SIZE_Y = 15;
+	
+	
+}
