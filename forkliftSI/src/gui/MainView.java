7f13e54ee8a14e9cc41383abf7ed07149178a3a1
Patryk Augustyn <patryk.augustyn@gmail.com>
2015-04-29 22:29:55 +0200

    -add logPlace (TextArea)

--- forkliftSI/src/gui/MainView.java
+++ forkliftSI/src/gui/MainView.java
@@ -1,5 +1,6 @@
 package gui;
 
+import java.awt.Container;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,68 +8,68 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import model.Forklift;
 import net.miginfocom.swing.MigLayout;
 
-
 public class MainView {
 
-	GridPanel gridPanel = new GridPanel();
-	static Forklift forklift = new Forklift();
+    GridPanel gridPanel = new GridPanel();
+    static Forklift forklift = new Forklift();
 
-	JLabel forkliftStateLabel = new JLabel("-- Stan poczπtkowy wÛzka -- ");
-	JLabel fuelLevelLabel = new JLabel("Poziom paliwa : ");
-	public static JLabel fuelLevel = new JLabel(Integer.toString(forklift.getFuelLevel()));
-	JLabel capacityLevelLabel = new JLabel("Obecne za≥adowanie : ");
-	public static JLabel capacityLevel = new JLabel(Integer.toString(forklift.getCapacity()));
+    JLabel forkliftStateLabel = new JLabel("-- Stan poczƒÖtkowy w√≥zka -- ");
+    JLabel fuelLevelLabel = new JLabel("Poziom paliwa : ");
+    public static JLabel fuelLevel = new JLabel(Integer.toString(forklift.getFuelLevel()));
+    JLabel capacityLevelLabel = new JLabel("Obecne za≈Çadowanie : ");
+    public static JLabel capacityLevel = new JLabel(Integer.toString(forklift.getCapacity()));
 
-	JButton refresh = new JButton("Odswiez");
+    JButton refresh = new JButton("Odswiez");
+    Container infoPlace = new Container();
+    JTextArea logPlace = new JTextArea(20,40);
 
-	public MainView() {
-		EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-				} catch (ClassNotFoundException ex) {
-				} catch (InstantiationException e2) {
-				} catch ( IllegalAccessException e3) {
-				} catch ( UnsupportedLookAndFeelException e4) {
-				}
-				
-				initializeListeners();
-				
-				JFrame frame = new JFrame("Inteligentny WÛzek wid≥owy");
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setLayout(new MigLayout());
+    public MainView() {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+                }
 
-				frame.add(forkliftStateLabel, "cell 0 0");
-				frame.add(fuelLevelLabel, "cell 0 1");
-				frame.add(fuelLevel, "w 80!, cell 0 1");
-				frame.add(capacityLevelLabel, "cell 0 2");
-				frame.add(capacityLevel, "cell 0 2");
+                initializeListeners();
+                JScrollPane scroll = new JScrollPane(logPlace);
+                logPlace.setEditable(false);
+                JFrame frame = new JFrame("Inteligentny W√≥zek wid≈Çowy");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLayout(new MigLayout());
 
-				frame.add(refresh, "cell 0 9, wrap");
-				frame.add(gridPanel, "east");
-				frame.pack();
-				frame.setLocationRelativeTo(null);
-				frame.setVisible(true);
-			}
-		});
-	}
-	
+                frame.add(forkliftStateLabel, "cell 0 0");
+                frame.add(fuelLevelLabel, "cell 0 1");
+                frame.add(fuelLevel, "w 80!, cell 0 1");
+                frame.add(capacityLevelLabel, "cell 0 2");
+                frame.add(capacityLevel, "cell 0 2");
+                frame.add(refresh, "cell 0 9, wrap");
+                frame.add(logPlace, "east");
+                frame.add(gridPanel, "east");
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+        });
+    }
 
-	private void initializeListeners() {
-		
-		refresh.addActionListener(new ActionListener() {
+    private void initializeListeners() {
 
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				gridPanel.clearGridView();
-			}
-		});
-	}
+        refresh.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                gridPanel.clearGridView();
+            }
+        });
+    }
 }
